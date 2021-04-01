ccba9e0f1262de05001a8a487aec4a448d8999a8
Mrinal Vig <mrinalvig@hotmail.com>
2021-04-01 00:34:59 -0700

    finished heroku databasing

--- db/index.js
+++ db/index.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'coolmkov',
-  database: 'products',
+  host: "lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "qp5i1cidoebadl7h",
+  password: "a4syn5cg3mo7ytp9",
+  database: "s2n6jva7nr1t2wlu",
 });
 
 connection.connect((err) => {
   if (err) {
     console.error(err);
+    console.log('uh-oh');
   } else {
     console.log('Connected to DB');
   }
